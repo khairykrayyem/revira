e1cd83e6e595d0a88ed2f9687dc0f01f59a58a3c
Khairy Krayyem <khairy.krayyem.2002@gmail.com>
2026-03-13 22:19:10 +0200

    update CSS

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
+import "./styles/base.css";
+import "./styles/home.css";
+import "./styles/booking.css";
+import "./styles/admin.css";
+import "./styles/responsive.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
